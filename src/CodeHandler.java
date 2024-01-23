204dfb2bbaeed5dafe7b6f60a59f09ac684efd81
donnylp <dondariolapointe8@gmail.com>
2024-01-23 16:57:32 -0500

    Implement CodeHandler functions

--- src/CodeHandler.java
+++ src/CodeHandler.java
@@ -3,40 +3,46 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class CodeHandler{
+public class CodeHandler {
 
-        private String basicFile;
-        private int index;
+    private final String  basicFile;
+    private int index;
 
-    
-        public CodeHandler(String fileName) throws IOException{
-            Path myPath = Paths.get(fileName);
-            this.basicFile = new String(Files.readAllBytes(myPath));
-        }
+    public CodeHandler(String fileName) throws IOException {
+        Path myPath = Paths.get(fileName);
+        this.basicFile = new String(Files.readAllBytes(myPath));
+    }
 
-        // TODO : This looks "i" characters ahead and returns that character; doesn't move the index
-        public char Peek(int i){
-            return 'p';
-        }
-
-        // TODO : This will return a string of the next "ith" character, but doesn't move the index
-        public String peekString(int i){
-            return "";
-        }
-
-        // TODO : moves the index ahead "i" positions
-        public void swallow(int i){
-
-        }
-        // TODO : returns true if we are at the end of the document
-        public boolean isDone(){
-            return false;
-        }
-
-        //TODO : returns the rest of the document as a string
-        public String remainder(){
-            return "";
-        }
+    public char peek(int i) {
+        return basicFile.charAt(i);
+    }
 
 
+    // move the index
+    public String peekString(int i) {
+        return this.basicFile.substring(index, i);
+    }
+
+
+    public void swallow(int i) {
+        this.index += i;
+    }
+
+
+    public boolean isDone() {
+        return index == this.basicFile.length();
+    }
+
+
+    public String remainder() {
+        return this.basicFile.substring(this.index);
+    }
+
+    public int getIndex(){
+        return this.index;
+    }
+
+    public int getFileLength(){
+        return this.basicFile.length();
+    }
 }
