aa407512cb490afe1df96ee334deb6b78c3ae626
donnylp <dondariolapointe8@gmail.com>
2024-01-23 18:47:15 -0500

    Add functionality to get file from args and print list of tokens

--- src/Basic.java
+++ src/Basic.java
@@ -1,13 +1,25 @@
-
+import java.io.IOException;
+import java.util.LinkedList;
 
 public class Basic {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        
-        if (args.length != 1){
+        if (args.length != 1) {
             System.out.println("Error: There should at least or at most, 1 file passed!");
             System.exit(0);
         }
-        // TODO : pass the filename into the lexer instance
+
+        String fileName = args[0];
+        Lexer lexer = new Lexer(fileName);
+
+        LinkedList<Token> output = lexer.lex();
+
+        // TO DO : Print each token out from lex method
+        int counter = 0;
+        while(!output.isEmpty()){
+            System.out.println(output.get(counter).toString());
+            counter++;
+        }
+
     }
 }
