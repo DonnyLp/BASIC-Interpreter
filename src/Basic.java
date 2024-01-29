f3e19d0ce618249e4f4220537f9301f9466fd13c
Dondario <dondariolapointe8@gmail.com>
2024-01-28 21:53:02 -0500

    Fixed print logic for tokens

--- src/Basic.java
+++ src/Basic.java
@@ -5,7 +5,7 @@ public class Basic {
     public static void main(String[] args) throws IOException {
 
         if (args.length != 1) {
-            System.out.println("Error: There should at least or at most, 1 file passed!");
+            System.out.println("Error: There should at least or at most, 1 file passed");
             System.exit(0);
         }
 
@@ -13,13 +13,10 @@ public class Basic {
         Lexer lexer = new Lexer(fileName);
 
         LinkedList<Token> output = lexer.lex();
+        Token [] tokens = output.toArray(new Token[0]);
 
-        // TO DO : Print each token out from lex method
-        int counter = 0;
-        while(!output.isEmpty()){
-            System.out.println(output.get(counter).toString());
-            counter++;
-        }
-
+       for(Token token: tokens){
+           System.out.println(token);
+       }
     }
 }
