1fb66125751c0dd8f84a6aa9d31884a3ce5cbedc
Dondario <dondariolapointe8@gmail.com>
2024-02-02 14:48:45 -0500

    Added helper methods for character validation and fixed string out of bounds bugs

--- src/Lexer.java
+++ src/Lexer.java
@@ -3,12 +3,9 @@ import java.util.LinkedList;
 
 public class Lexer {
 
-    private LinkedList<Token> tokens = new LinkedList<>();
-    private CodeHandler handler;
-    private Token token;
-
+    private final LinkedList<Token> tokens = new LinkedList<>();
+    private final CodeHandler handler;
     private int lineNumber;
-
     private int characterPosition;
 
     private int characterOffset;
@@ -17,112 +14,112 @@ public class Lexer {
         WORD,
         NUMBER,
         IGNORE,
+        INVALID,
         ENDOFLINE,
 
     }
 
     public Lexer(String fileName) throws IOException {
         handler = new CodeHandler(fileName);
-        // TO DO: instantiate the linenumber and character position
+        // TO DO: instantiate the line number and character position
         this.lineNumber = 1;
-        this.characterPosition = 0;
+        this.characterPosition = 1;
         this.characterOffset = 0;
     }
 
-    // TODO : Finish the remaining requirements for this method
     public LinkedList<Token> lex() {
 
-        // TO DO: Loop While there's still data in the code handler
-
-        /*
-
-            U:Use state machine w/helper method to identify the state
-            - We iterate through the file
-            - make tokens by the cases defined above
-            M:
-                - While loop to iterate through the file
-                - switch case to account for all cases above
-                - handle logic for each case
-                - create new token to the LinkedList of tokens
-                - move the index with swallow()?
-         */
-        int i = 0;
         while (!handler.isDone()) {
 
-            i = handler.getIndex();
-            char current = handler.peek(i);
+            int currentIndex = handler.getIndex();
+            char current = handler.peek(currentIndex);
 
+            //State machine like implementation to control each character case
             switch (matchState(current)) {
-                case IGNORE -> handler.swallow(1);
+                case IGNORE -> {
+                    if(current == ' '){
+                        this.characterPosition++;
+                    }
+                    if(current == '\t'){
+                        this.characterPosition += 4;
+                    }
+
+                    handler.swallow(1);
+                }
                 case WORD -> tokens.add(processWord(handler));
                 case NUMBER -> tokens.add(processNumber(handler));
                 case ENDOFLINE -> {
                     tokens.add(new Token(Token.TokenType.ENDOFLINE, this.lineNumber, this.characterPosition));
                     this.lineNumber += 1;
-                    this.characterPosition = 0;
+                    this.characterPosition = 1;
                     this.characterOffset = handler.getIndex();
                     handler.swallow(1);
                 }
-                default -> System.out.println("Unexpected character intercepted");
-
+                case INVALID -> throw new IllegalArgumentException(invalidCharMessage("Invalid character", this.lineNumber, this.characterPosition));
             }
         }
         return tokens;
     }
 
 
+    //Accepts letters,numbers, and string terminators for string variables
     private Token processWord(CodeHandler handler) {
 
         StringBuilder tokenValue = new StringBuilder();
-        Token token = new Token("", Token.TokenType.WORD, this.lineNumber, this.characterPosition);
-        int i = handler.getIndex();
+        Token token;
+        int currentIndex = handler.getIndex();
 
-        //collect characters that are acceptable for the "WORD" token
-        while (!handler.isDone() && (Character.isAlphabetic(handler.peek(i)) || handler.peek(i) == '$' || handler.peek(i) == '%')) {
-            tokenValue.append(handler.peek(i));
+        //Loop through characters while it's either a letter, digit, or terminating symbol for string variables
+        while (!handler.isDone() && (isValidWordChar(handler.peek(currentIndex)))){
+            tokenValue.append(handler.peek(currentIndex));
+            currentIndex++;
             handler.swallow(1);
-            i++;
         }
 
         token = new Token(tokenValue.toString(), Token.TokenType.WORD, this.lineNumber, this.characterPosition);
-        this.characterPosition = i - characterOffset;
-        handler.swallow(i - handler.getIndex());
+        this.characterPosition = currentIndex - characterOffset;
+        handler.swallow(currentIndex - handler.getIndex());
         return token;
     }
-    // TO DO : Create the ProcessNumber method that returns a token and accepts 0 - 9 and one decimal
-    /*
-    U: process the characters and form a token with numbers and/or decimal number
-    M:
-    - Loop while: char is number or char is decimal
-        - if current char is decimal point and isDecimal is true
-        print "Syntax error: too many decimals"
-        - token.append(handler.peek(i));
-    - return new Token (token, Number, lineNumber, characterPosition)
-     */
 
-    // TO DO: Fix logic for single decimal and rest of the method
+    //Accepts number & decimals
     private Token processNumber(CodeHandler handler) {
-        StringBuilder tokenValue = new StringBuilder();
-        Token token = new Token("", Token.TokenType.NUMBER, this.lineNumber, this.characterPosition);
-        boolean isDecimal = false;
-        int i = handler.getIndex();
-        while (Character.isDigit(handler.peek(i)) || handler.peek(i) == '.') {
-            //TO DO: Fix logic for single decimal
-            //handle logic for single decimal
-            if (handler.peek(i) == '.') isDecimal = true;
-            if (handler.peek(i) == '.' && isDecimal) {
-                System.out.println("Syntax error: too many decimals");
-                break;
-            }
-                tokenValue.append(i);
-            }
-            return new Token("", Token.TokenType.NUMBER, this.lineNumber, this.characterPosition);
-        }
 
-    //helper method to match input char to specific state
+        StringBuilder tokenValue = new StringBuilder();
+        Token token;
+        boolean isDecimal = false;
+        int currentIndex = handler.getIndex();
+
+        //Loops through characters while they are digits or at decimal points
+        while (!handler.isDone() && (Character.isDigit(handler.peek(currentIndex)) || handler.peek(currentIndex) == '.' || handler.peek(currentIndex) == ' ')) {
+
+            //Handle invalid character exception
+            if(isValidNumberChar(handler.peek(currentIndex))){
+                throw new IllegalArgumentException(invalidCharMessage("Invalid character", this.lineNumber,this.characterPosition));
+            }
+
+            //Handles the case where there's more than one decimal detected
+            if (handler.peek(currentIndex) == '.' && isDecimal){
+                throw new IllegalArgumentException(invalidCharMessage("Extra Decimal", this.lineNumber, this.characterPosition));
+            }
+
+            if (handler.peek(currentIndex) == '.') isDecimal = true;
+
+            tokenValue.append(handler.peek(currentIndex));
+            currentIndex++;
+            handler.swallow(1);
+        }
+        //Create new token with accumulated string of characters, update the character position and swallow the characters of the recent token
+        token = new Token(tokenValue.toString(), Token.TokenType.NUMBER, this.lineNumber, this.characterPosition);
+        this.characterPosition = currentIndex - characterOffset;
+        handler.swallow(currentIndex - handler.getIndex());
+        return token;
+    }
+
+    //Helper method to match character to specific token state (WORD, NUMBER,etc.)
     private state matchState(char current) {
 
-        state newState = null;
+        state newState = state.INVALID;
 
         if (Character.isSpaceChar(current) || current == '\t' || current == '\r') newState = state.IGNORE;
         else if (Character.isAlphabetic(current)) newState = state.WORD;
@@ -130,5 +127,19 @@ public class Lexer {
         else if (current == '\n') newState = state.ENDOFLINE;
 
         return newState;
+    }
+
+    //Helper for processWord tokenizing
+    private boolean isValidWordChar(char c){
+        return Character.isAlphabetic(c) || Character.isDigit(c) || c == '$' || c == '%';
+    }
+
+    private boolean isValidNumberChar(char c){
+        return Character.isDigit(c) || c == '.';
+    }
+
+    //Helper method to create custom error message with specifications
+    private String invalidCharMessage(String customMessage, int lineNumber, int characterPosition){
+        return String.format("%s at Line: %d, Character %d", customMessage, lineNumber, characterPosition);
     }
 }
