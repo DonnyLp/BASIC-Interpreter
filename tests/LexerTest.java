4693544e4004a33bffbafaaaf1c4c36b37bc2c4e
Dondario <dondariolapointe8@gmail.com>
2024-02-12 22:06:15 -0500

    Modified lexer test for keywords, numbers, words, symbols, and string literals

--- tests/LexerTest.java
+++ tests/LexerTest.java
@@ -1,68 +1,70 @@
-import org.junit.Test;
-
 import java.io.IOException;
 import java.util.LinkedList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+
 
 public class LexerTest {
 
     @Test
     public void lexTest() throws IOException {
         Lexer lexer = new Lexer("test.txt");
-        LinkedList<Token> tokens;
-        tokens = lexer.lex();
-
-        assertEquals(9, tokens.size());
-
-        //first token
-        Token first = tokens.getFirst();
-        assertEquals(Token.TokenType.WORD, first.getType());
-        assertEquals("is",first.getTokenValue());
-
-        //second token
-        Token second = tokens.get(1);
-        assertEquals(Token.TokenType.NUMBER, second.getType());
-        assertEquals("123",second.getTokenValue());
-
-        //third token
-        Token third = tokens.get(2);
-        assertEquals(Token.TokenType.WORD, third.getType());
-        assertEquals("and",third.getTokenValue());
-
-        //fourth token
-        Token fourth = tokens.get(3);
-        assertEquals(Token.TokenType.NUMBER, fourth.getType());
-        assertEquals("1.23",fourth.getTokenValue());
-
-        //fifth token
-        Token fifth = tokens.get(4);
-        assertEquals(Token.TokenType.WORD, fifth.getType());
-        assertEquals("numbers",fifth.getTokenValue());
-
-        //fifth token
-        Token sixth = tokens.get(5);
-        assertEquals(Token.TokenType.ENDOFLINE, sixth.getType());
-
-        //seventh token
-        Token seventh = tokens.get(6);
-        assertEquals(Token.TokenType.WORD, seventh.getType());
-        assertEquals("invalid", seventh.getTokenValue());
-
-        //eighth token
-        Token eighth = tokens.get(7);
-        assertEquals(Token.TokenType.NUMBER, eighth.getType());
-        assertEquals("1.2",eighth.getTokenValue());
-
-       //ninth token
-       Token ninth = tokens.get(8);
-       assertEquals(Token.TokenType.ENDOFLINE, ninth.getType());
+        LinkedList<Token> tokens = lexer.lex();
+        Token token = lexer.nexToken();
 
 
+        //WORD TEST
+
+        assertEquals(Token.TokenType.WORD, token.getType());
+        assertEquals("Test", token.getValue());
+        token = lexer.nexToken();
 
 
+        //SYMBOLS TEST
+        assertEquals(Token.TokenType.NOTEQUALS, token.getType());
+        assertEquals("<>", token.getValue());
+        token = lexer.nexToken();
 
+        assertEquals(Token.TokenType.LESSTHAN, token.getType());
+        assertEquals("<", token.getValue());
+        token = lexer.nexToken();
+
+        //NUMBERS TEST
+        assertEquals(Token.TokenType.NUMBER, token.getType());
+        assertEquals("3.1459", token.getValue());
+        token = lexer.nexToken();
+
+        //STRING LITERALS TEST
+
+        //empty string literal
+        assertEquals(Token.TokenType.STRINGLITERAL, token.getType());
+        assertEquals("\"\"", token.getValue());
+        token = lexer.nexToken();
+
+        //regular
+        assertEquals(Token.TokenType.STRINGLITERAL, token.getType());
+        assertEquals("\"Nice Test Here!\"", token.getValue());
+        token = lexer.nexToken();
+
+        //escaped
+        assertEquals(Token.TokenType.STRINGLITERAL, token.getType());
+        assertEquals("\"Sung \"DRIP\" WOO\"", token.getValue());
+        token = lexer.nexToken();
+
+        assertEquals(Token.TokenType.ENDOFLINE, token.getType());
+        token = lexer.nexToken();
+
+        //KEYWORDS TEST
+        assertEquals(Token.TokenType.FOR, token.getType());
+        token = lexer.nexToken();
+
+        assertEquals(Token.TokenType.STEP, token.getType());
+        token = lexer.nexToken();
+
+        assertEquals(Token.TokenType.ENDOFLINE, token.getType());
     }
-
-
 }
