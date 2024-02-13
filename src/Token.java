4ae02570b47f0ab194b783a33172b2ece0aff9dd
donnylp <dondariolapointe8@gmail.com>
2024-02-12 21:17:22 -0500

    Refactored some variables variable, added new token (COMMA) ,changed toString method for better readability, and added constructor for token and value.

--- src/Token.java
+++ src/Token.java
@@ -1,40 +1,44 @@
 public class Token {
     
-    private String tokenValue;
-    private TokenType token;
+    private String value;
+    private final TokenType token;
     private int lineNumber;
-    private int characterPos;
+    private int characterPosition;
 
     public enum TokenType{
         WORD, NUMBER, IF,DO, STRINGLITERAL, NOTEQUALS, LPAREN, RPAREN,
         EQUALS, LESSTHAN, GREATERTHAN, LESSTHANOREQUAL, GREATERTHANOREQUAL,
         ADD, SUBTRACT, MULTIPLY, DIVIDE,
         LABEL, PRINT, READ, INPUT, DATA, GOSUB,FOR, TO, STEP, NEXT, RETURN, THEN, FUNCTION, WHILE, END,
-        ENDOFLINE
+        COMMA, ENDOFLINE
     }
 
-
-    public Token(String tokenValue, TokenType token, int lineNumber, int characterPos){
-        this.tokenValue = tokenValue;
+    public Token(TokenType token, String value){
         this.token = token;
-        this.lineNumber = lineNumber;
-        this.characterPos = characterPos;
+        this.value = value;
     }
 
-    public Token(TokenType token, int lineNumber, int characterPos){
+    public Token(String value, TokenType token, int lineNumber, int characterPosition){
+        this.value = value;
         this.token = token;
         this.lineNumber = lineNumber;
-        this.characterPos = characterPos;
+        this.characterPosition = characterPosition;
+    }
+
+    public Token(TokenType token, int lineNumber, int characterPosition){
+        this.token = token;
+        this.lineNumber = lineNumber;
+        this.characterPosition = characterPosition;
 
     }
 
     public TokenType getType(){ return this.token; }
 
-    public String getTokenValue(){ return this.tokenValue; }
+    public String getValue(){ return this.value; }
 
 
     @Override
-    public String toString(){
-        return this.token + " : " + "\"" + this.tokenValue + "\" " + " ( " + "Line: " + this.lineNumber + " , " + "Character: " + this.characterPos + " ) ";
+    public String toString() {
+        return String.format("%s <%s> - (%d:%d)", token, value, lineNumber, characterPosition);
     }
 }
