44918c76d10138efa5c3adeeeb62b9863aa682a8
donnylp <dondariolapointe8@gmail.com>
2024-03-02 20:59:42 -0500

    Implemented to handle variables

---
+++ src/VariableNode.java
@@ -0,0 +1,13 @@
+public class VariableNode extends Node{
+    
+    private final String name;
+
+    public VariableNode(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
